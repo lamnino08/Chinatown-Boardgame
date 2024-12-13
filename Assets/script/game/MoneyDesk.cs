e541ff3ef7c59903fb094a0b3209334f41fe17cf
lamnino <phanduclam02@gmail.com>
2024-12-13 17:08:42 +0700

    money statck

--- Assets/script/game/MoneyDesk.cs
+++ Assets/script/game/MoneyDesk.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum MoneyType 
+{
+    TEN = 10,
+    FIFTY = 50,
+    HUNDRED = 100,
+    TWO_HUNDRED = 200
+}
 public class MoneyDesk : MonoBehaviour
 {
-    // [SerializeField]
+    public static MoneyDesk instance { get; private set; }
+    public Transform moneyCardPrefab;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
 }
--- Assets/script/game/MoneyStack.cs
+++ Assets/script/game/MoneyStack.cs
@@ -1,21 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoneyStack : MonoBehaviour
 {
     private static Vector3 distance = new Vector3(0, 0.03f, 0);
-    [SerializeField] private int money;
+    [SerializeField] private MoneyType type;
     [SerializeField] private int amount;
-    [SerializeField] private Transform moneyCardPref;
+    [SerializeField] private List<MoneyCard> _moneyCards = new List<MoneyCard>();
 
     public void Start()
     {
         Vector3 startPos = transform.position;
         for(int i = 1; i <= amount; i++)
         {
-            Transform card = Instantiate(moneyCardPref, transform);
+            Transform card = Instantiate(MoneyDesk.instance.moneyCardPrefab, transform.position + distance * i, transform.rotation);
             card.position = startPos + distance * i;
+
         }
     }
 }
